dd1690f5225fe0a1f61b7f3fd8a1533e6d1ffa71
David Andrés Valencia García <109925924+devBorisG@users.noreply.github.com>
2023-08-24 17:36:08 -0500

    Update project

---
+++ dtoYourPlus/src/main/java/com/uco/yourplus/dtoyourplus/DtoYourPlusApplication.java
@@ -0,0 +1,13 @@
+package com.uco.yourplus.dtoyourplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtoYourPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtoYourPlusApplication.class, args);
+	}
+
+}
---
+++ entityYourPlus/src/main/java/com/uco/yourplus/entityyourplus/EntityYourPlusApplication.java
@@ -0,0 +1,13 @@
+package com.uco.yourplus.entityyourplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityYourPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntityYourPlusApplication.class, args);
+	}
+
+}
