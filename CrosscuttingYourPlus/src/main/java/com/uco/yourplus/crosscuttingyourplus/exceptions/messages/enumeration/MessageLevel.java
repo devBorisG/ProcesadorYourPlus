ba045510ea41ad938eba60f45fe90fa6bd19a884
nrl0315 <nicolas.rendon5948@soyuco.edu.co>
2023-09-26 10:02:44 -0500

    Creación del crosscutting para procesador

---
+++ CrosscuttingYourPlus/src/main/java/com/uco/yourplus/crosscuttingyourplus/exceptions/messages/enumeration/MessageLevel.java
@@ -0,0 +1,29 @@
+package com.uco.yourplus.crosscuttingyourplus.exceptions.messages.enumeration;
+
+public enum MessageLevel {
+
+    /**
+     * Nivel de mensaje FATAL, que indica un error grave que generalmente resulta en la terminación de la aplicación.
+     */
+    FATAL,
+
+    /**
+     * Nivel de mensaje ERROR, que indica un error que afecta negativamente el funcionamiento de la aplicación.
+     */
+    ERROR,
+
+    /**
+     * Nivel de mensaje WARNING, que indica una advertencia o una condición potencialmente problemática.
+     */
+    WARNING,
+
+    /**
+     * Nivel de mensaje INFO, que proporciona información general sobre el estado o el progreso de la aplicación.
+     */
+    INFO,
+
+    /**
+     * Nivel de mensaje SUCCESS, que indica un mensaje de éxito o confirmación.
+     */
+    SUCCESS
+}
---
+++ CrosscuttingYourPlus/src/main/java/com/uco/yourplus/crosscuttingyourplus/helper/IntegerHelper.java
@@ -0,0 +1,26 @@
+package com.uco.yourplus.crosscuttingyourplus.helper;
+
+import static com.uco.yourplus.crosscuttingyourplus.helper.ObjectHelper.getDefaultIfNull;
+
+public class IntegerHelper {
+
+    // Evita la instanciación de la clase, ya que es de utilidad y no debe crearse instancias.
+    private IntegerHelper(){
+        super();
+    }
+
+    /**
+     * Valor constante que representa el entero cero (0).
+     */
+    public static final int ZERO = 0;
+
+    /**
+     * Obtiene el valor entero proporcionado o el valor predeterminado si es nulo.
+     *
+     * @param value El valor entero que se verificará.
+     * @return El valor entero proporcionado o el valor predeterminado si es nulo.
+     */
+    public static int getDefaultInteger(int value){
+        return getDefaultIfNull(value,ZERO);
+    }
+}
