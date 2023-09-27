4e3a42679a6b661548c7bdf8e685654b7c482dff
David Andrés Valencia García <david.andres.2801@gmail.com>
2023-09-26 19:38:04 -0500

    Construccion interfaz specification Pattern

---
+++ ServiceYourPlus/src/main/java/com/uco/yourplus/serviceyourplus/specification/producto/RegistrarProductoSpecification.java
@@ -0,0 +1,13 @@
+package com.uco.yourplus.serviceyourplus.specification.producto;
+
+import com.uco.yourplus.serviceyourplus.domain.ProductoDomain;
+import com.uco.yourplus.serviceyourplus.specification.ISpecification;
+
+public class RegistrarProductoSpecification implements ISpecification<ProductoDomain> {
+
+
+    @Override
+    public void isSatisfied(ProductoDomain domain) {
+
+    }
+}
