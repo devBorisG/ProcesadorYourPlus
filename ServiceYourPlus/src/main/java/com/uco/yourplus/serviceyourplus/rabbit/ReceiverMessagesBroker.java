c66ca0583650a2df8b167ad3c3241a3733892050
David Andrés Valencia García <david.andres.2801@gmail.com>
2023-10-04 23:08:53 -0500

    commit

--- ServiceYourPlus/src/main/java/com/uco/yourplus/serviceyourplus/rabbit/ReceiverMessagesBroker.java
+++ ServiceYourPlus/src/main/java/com/uco/yourplus/serviceyourplus/rabbit/ReceiverMessagesBroker.java
@@ -1,7 +1,6 @@
 package com.uco.yourplus.serviceyourplus.rabbit;
 
 import com.uco.yourplus.serviceyourplus.usecase.producto.RegistrarProducto;
-import com.uco.yourplus.serviceyourplus.util.MapperJsonObjeto;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,12 +10,6 @@ public class ReceiverMessagesBroker {
 
     @Autowired
     RegistrarProducto registrarProducto;
-
-    private final MapperJsonObjeto mapperJsonObjeto;
-
-    public ReceiverMessagesBroker(MapperJsonObjeto mapperJsonObjeto){
-        this.mapperJsonObjeto = mapperJsonObjeto;
-    }
 
     @RabbitListener(queues = "${yourplus.management.producto.queue.save}")
     public void receiverMessagesProcessClient(String message) {
